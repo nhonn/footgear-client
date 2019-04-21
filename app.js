54cfbdac9aa4083e9384a58dfbdf9158c67a0d8d
Nhon Nguyen <nhonnt97@gmail.com>
2019-04-21 23:36:08 +0700

    add cart, category & detail views

--- app.js
+++ app.js
@@ -36,12 +36,12 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+  res.status(404).render("error404");
 });
 
 // error handler
-app.use(function (err, req, res) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
