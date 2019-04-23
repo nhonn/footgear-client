469a3697299fcc76034ead6b89b603a3bd72abd4
Nhon Nguyen <nhonnt97@gmail.com>
2019-04-23 14:12:59 +0700

    add user views

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/login", function(req, res, next) {
+  res.render("login", { title: "Đăng nhập" });
+});
+
+router.get("/register", function(req, res, next) {
+  res.render("register", { title: "Đăng ký" });
+});
+
+router.get("/profile", function(req, res, next) {
+  res.render("profile", { title: "Thông tin tài khoản" });
 });
 
 module.exports = router;
